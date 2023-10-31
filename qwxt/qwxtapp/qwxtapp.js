b44977028649af789285938db91ee3ad64f7162f
KetiElizbarashvili <elizbarashviliketi44@gmail.com>
2023-10-31 18:59:53 +0400

    finished

--- qwxt/qwxtapp/qwxtapp.js
+++ qwxt/qwxtapp/qwxtapp.js
@@ -2,15 +2,12 @@ var txtsend = document.getElementById('txtsend');
 
 var app = document.getElementById('responses');
 app.addEventListener('click', function (e) {
-    //console.log(e);
 
     var clickedItem = e.target;
 
     var clickclass = clickedItem.getAttribute("class");
-    //console.log(clickclass);
 
     if (clickclass == "btn") {
-        //console.log(clickedItem.innerHTML);
         txtsend.innerHTML = clickedItem.innerHTML;
         txtsend.setAttribute("class", "pstyle bluemessage");
     }
